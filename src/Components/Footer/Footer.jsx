46363e4e2d9a48c474ff4c30abb78c92466bb489
Evelin Romero <Evelinrocio2816@gmail.com>
2023-10-15 22:24:12 -0300

    Final

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,7 +6,14 @@ const Footer = () => {
    <footer>
   <p className="copyRight">  &copy;Copyright 2023 - EvelinRocio2816</p>
 
+<div className="imgFooter">
+   <img className="image-footer" src="../img/whatsapp.png" alt="Whatsapp" />
+   <img className="image-footer" src="../img/instagram.png" alt="Instagram" />
+   <img className="image-footer" src="../img/gmail.png" alt="Gmail" />
+   <img className="image-footer" src="../img/facebook.png" alt="Facebook" />
+   </div>
    <Logo/>
+   
    
    </footer>
   )
